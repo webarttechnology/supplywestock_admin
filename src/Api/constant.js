df150d0b2659dec27d73621e24518d39e7f3b4f4
iamwebartdevteam <biswajit@webart.technology>
2022-12-13 09:49:45 +0530

    https add

--- src/Api/constant.js
+++ src/Api/constant.js
@@ -1,9 +1,9 @@
 export const CURRENCY = "$";
-export const IMG = "http://api.supplywestock.com/";
+export const IMG = "https://api.supplywestock.com/";
 export const TIMEZONE = "Asia/Calcutta";
 // ? ====== BASE URL ======
-export const URL = "http://api.supplywestock.com:3001/v1";
-export const SOCEKT = "http://api.supplywestock.com:3001";
+export const URL = "https://api.supplywestock.com:3001/v1";
+export const SOCEKT = "https://api.supplywestock.com:3001";
 //export const URL = "http://192.168.1.182:3001/v1";
 export const ADMIN = `${URL}/author`;
 export const MENUFACTURS = `${URL}/manufacturers`;
--- src/page/Message.js
+++ src/page/Message.js
@@ -1,62 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import Messagefedd from '../components/Messagefedd'
-import MessSidBar from '../components/MessSidBar'
+import React, { useEffect, useState } from "react";
+import Messagefedd from "../components/Messagefedd";
+import MessSidBar from "../components/MessSidBar";
 import { io } from "socket.io-client";
-import { URL } from '../Api/constant';
+import { URL } from "../Api/constant";
 import * as API from "../Api/index";
 const initial = {
-message: "",
+  message: "",
 };
 
 const Message = () => {
- const socket = io("http://api.supplywestock.com:3001");
- console.log("socket", socket);
-    const [userList, setUserList] = useState([])
-    const [feedMess, setFeedMess] = useState([])
-    const [formData, setFormData] = useState(initial)
-    const [userDetails, setUserDetails] = useState([])
-    const [chatCode, setChatCode] = useState("")
-    const [text, setText] = useState("");
-    const [typeData, setTypeData] = useState("")
-    const [typeUserid, setTypeUserid] = useState("")
-    const [typeId, setTypeId] = useState("")
-    const [sallerid, setSallerid] = useState("")
-    const messageHandaler = (data) => {  
-        socket.emit('typing', {user: data === "" ? "" : localStorage.getItem("_userId"), typing: data === "" ? false : true})
-        setText(data)
-    }
+  const socket = io("https://api.supplywestock.com:3001");
+  console.log("socket", socket);
+  const [userList, setUserList] = useState([]);
+  const [feedMess, setFeedMess] = useState([]);
+  const [formData, setFormData] = useState(initial);
+  const [userDetails, setUserDetails] = useState([]);
+  const [chatCode, setChatCode] = useState("");
+  const [text, setText] = useState("");
+  const [typeData, setTypeData] = useState("");
+  const [typeUserid, setTypeUserid] = useState("");
+  const [typeId, setTypeId] = useState("");
+  const [sallerid, setSallerid] = useState("");
+  const messageHandaler = (data) => {
+    socket.emit("typing", {
+      user: data === "" ? "" : localStorage.getItem("_userId"),
+      typing: data === "" ? false : true,
+    });
+    setText(data);
+  };
 
-    const chatRoomShow = async() =>{
-        const header = localStorage.getItem("_tokenCode");
-        try {
-            const response = await API.chatRoomlist(localStorage.getItem("_userId"), header) 
-            console.log("responsedf", response);
-            setUserList(response.data.data)
-        } catch (error) {
-            
-        }
-   } 
+  const chatRoomShow = async () => {
+    const header = localStorage.getItem("_tokenCode");
+    try {
+      const response = await API.chatRoomlist(
+        localStorage.getItem("_userId"),
+        header
+      );
+      console.log("responsedf", response);
+      setUserList(response.data.data);
+    } catch (error) {}
+  };
 
+  const chatShowIng = async (chatCodeid, userDet, user) => {
+    setUserDetails(userDet);
+    console.log("user", user);
+    user.map((item, index) =>
+      item.roleId === "2" ? setSallerid(item.userCode) : ""
+    );
+    setChatCode(chatCodeid);
+    const header = localStorage.getItem("_tokenCode");
+    try {
+      const response = await API.chatfeedShow(chatCodeid, header);
+      console.log("sssresponse", response);
+      setFeedMess(response.data.data);
+    } catch (error) {}
+  };
 
-   const chatShowIng = async (chatCodeid, userDet, user) => {
-        setUserDetails(userDet)
-        console.log("user", user);
-        user.map((item, index)=>(
-            item.roleId === "2" ? setSallerid(item.userCode) : ""
-          ))
-        setChatCode(chatCodeid)
-        const header = localStorage.getItem("_tokenCode");
-        try {
-            const response = await API.chatfeedShow(chatCodeid, header)
-            console.log("sssresponse", response);
-            setFeedMess(response.data.data)
-        } catch (error) {
-            
-        }
-   }  
-
-
-   const handalerChanges = (e) => {
+  const handalerChanges = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
@@ -64,77 +64,75 @@ const Message = () => {
   function handleOnEnter(text) {
     console.log("chatCode", chatCode);
     socket.emit("createChat", {
-        senderId: localStorage.getItem("_userId"),
-        chatroomId: chatCode,
-        message: text,
+      senderId: localStorage.getItem("_userId"),
+      chatroomId: chatCode,
+      message: text,
     });
   }
 
   const messageAccept = async () => {
     console.log("jdfd");
+  };
 
-  }
-   
   const regectMessage = async () => {
     console.log("regectMessage");
-  }
+  };
 
-   useEffect(() => {
-        socket.on("display", (data) => {
-            console.log("display", data);
-            setTypeData(data.typing)
-            setTypeUserid(data.chatCode)
-            setTypeId(data.user)
-        });
+  useEffect(() => {
+    socket.on("display", (data) => {
+      console.log("display", data);
+      setTypeData(data.typing);
+      setTypeUserid(data.chatCode);
+      setTypeId(data.user);
+    });
 
-        socket.on("receiveChat", (data) => {
-            console.log("receiveChat", data);
-            setFeedMess(data);
-        });
-        chatRoomShow()
-    }, [])
+    socket.on("receiveChat", (data) => {
+      console.log("receiveChat", data);
+      setFeedMess(data);
+    });
+    chatRoomShow();
+  }, []);
 
   return (
     <>
-        <section class="section">
-            <div class="page-heading">
-                <h3>Message</h3>
-            </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div className='row'>
-                                <div className='col-md-3 border_right'>
-                                    <MessSidBar userList={userList} chatShowIng={chatShowIng} />
-                                </div>
-                                <div className='col-md-9'>
-                                    {userDetails.length === 0 ? (
-                                        ""
-                                    ):(
-                                        <Messagefedd 
-                                            handalerChanges={handalerChanges}
-                                            userDetails={userDetails}
-                                            setText={messageHandaler} 
-                                            feedMess={feedMess}
-                                            text={text}
-                                            typeData={typeData}
-                                            messageSend={handleOnEnter}
-                                            regectMessage={regectMessage}
-                                            messageAccept={messageAccept}
-                                            sallerid={sallerid}
-                                        />
-                                    )}
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <section class="section">
+        <div class="page-heading">
+          <h3>Message</h3>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <div class="card">
+              <div class="card-body">
+                <div className="row">
+                  <div className="col-md-3 border_right">
+                    <MessSidBar userList={userList} chatShowIng={chatShowIng} />
+                  </div>
+                  <div className="col-md-9">
+                    {userDetails.length === 0 ? (
+                      ""
+                    ) : (
+                      <Messagefedd
+                        handalerChanges={handalerChanges}
+                        userDetails={userDetails}
+                        setText={messageHandaler}
+                        feedMess={feedMess}
+                        text={text}
+                        typeData={typeData}
+                        messageSend={handleOnEnter}
+                        regectMessage={regectMessage}
+                        messageAccept={messageAccept}
+                        sallerid={sallerid}
+                      />
+                    )}
+                  </div>
                 </div>
+              </div>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
